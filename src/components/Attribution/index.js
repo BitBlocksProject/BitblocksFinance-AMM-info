2b5923d9c41d9c5e4b1e0077c748fdcb9be9c8d3
Edson Guimaraes <edsoneto.50@gmail.com>
2022-05-01 13:11:23 -0300

    BitBlocksFinance analytics

---
+++ src/components/Attribution/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Attribution = () => (
+  <p className="attribution">
+    <a href="https://github.com/bitblocksproject" rel="noopener noreferrer" target="_blank">
+      Github
+    </a>{' '}
+    |{' '}
+    <a href="https://bitblocks.finance" rel="noopener noreferrer" target="_blank">
+      BitBlocksFinance
+    </a>{' '}
+    |{' '}
+    <a href="https://gifer.com/en/1Atv" rel="noopener noreferrer" target="_blank">
+      GIF
+    </a>
+  </p>
+)
+
+export default Attribution
---
+++ src/constants/index.js
@@ -0,0 +1,23 @@
+export const FACTORY_ADDRESS = '0x65b3cc7a7cb167221266fc93884717de2dbd074e'
+
+export const BUNDLE_ID = '1'
+
+export const timeframeOptions = {
+  WEEK: '1 week',
+  MONTH: '1 month',
+  // THREE_MONTHS: '3 months',
+  // YEAR: '1 year',
+  ALL_TIME: 'All time',
+}
+
+// token list urls to fetch tokens from - use for warnings on tokens and pairs
+export const SUPPORTED_LIST_URLS__NO_ENS = [
+  'https://raw.githubusercontent.com/BitBlocksProject/bbkfi-router-frontend/main/src/constants/token/pancakeswap.json?token=GHSAT0AAAAAABT7VNP227NHNCPCZYKTKGL4YTOV5DQ',
+]
+
+// hide from overview list
+export const OVERVIEW_TOKEN_BLACKLIST = [
+]
+
+// pair blacklist
+export const PAIR_BLACKLIST = []
